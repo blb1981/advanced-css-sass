c67ce66d6afc1e7070132e803de19a5586978ebe
Brandon <brandonbland90@gmail.com>
2022-05-24 09:34:05 -0400

    Added reference code, several updates

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,15 @@ import '../scss/main.scss'
 import logoWhite from '../natours/img/logo-white.png'
 const logoWhiteImg = document.getElementById('logoWhite')
 logoWhiteImg.src = logoWhite
+
+import composite1 from '../natours/img/nat-1-large.jpg'
+import composite2 from '../natours/img/nat-2-large.jpg'
+import composite3 from '../natours/img/nat-3-large.jpg'
+
+const composite1Img = document.getElementById('composite1')
+const composite2Img = document.getElementById('composite2')
+const composite3Img = document.getElementById('composite3')
+
+composite1Img.src = composite1
+composite2Img.src = composite2
+composite3Img.src = composite3
